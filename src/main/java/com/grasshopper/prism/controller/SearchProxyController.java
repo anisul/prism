c81cb4dc1e6f8cc40c894ee1a1481f5603f06a00
anisul islam <anis.2803@gmail.com>
2026-03-29 23:44:45 +0200

    add search.html for semantic search interface; implement search functionality and results display

--- src/main/java/com/grasshopper/prism/controller/SearchProxyController.java
+++ src/main/java/com/grasshopper/prism/controller/SearchProxyController.java
@@ -11,13 +11,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/search")
+@CrossOrigin(origins = "*")
 public class SearchProxyController {
 
     private static final Logger log = LoggerFactory.getLogger(SearchProxyController.class);
